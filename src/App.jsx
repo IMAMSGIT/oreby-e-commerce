26fa5863fe68c8b514d780c6f10a34c04ed0edf2
Imam Hossain <imamhosain95@gmail.com>
2024-03-13 01:00:38 +0600

    React Router DOM installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Rootlayout from "./components/layouts/Rootlayout";
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Rootlayout />}></Route>)
+);
 const App = () => {
-  return <div className="bg-red-500">App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
